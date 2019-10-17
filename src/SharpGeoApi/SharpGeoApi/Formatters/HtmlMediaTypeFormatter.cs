04a2efe461749ea25e67eb034305a67529fed16c
Bert Temme <btemme@hotmail.com>
2019-10-17 14:42:35 +0200

    adding text/html formatter

---
+++ src/SharpGeoApi/SharpGeoApi/Formatters/HtmlMediaTypeFormatter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGeoApi.Services.Formatters
+{
+    public class HtmlMediaTypeFormatter : TextOutputFormatter
+    {
+        public HtmlMediaTypeFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/html"));
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+
+        }
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var sb = new StringBuilder();
+
+            var obj = context.Object;
+            // todo: write obj to html representation
+            sb.Append("uyoyoy");
+            return context.HttpContext.Response.WriteAsync(sb.ToString());
+        }
+    }
+}
--- src/SharpGeoApi/SharpGeoApi/Startup.cs
+++ src/SharpGeoApi/SharpGeoApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SharpGeoApi.Services.Formatters;
 
 namespace SharpGeoApi
 {
@@ -26,9 +27,10 @@ namespace SharpGeoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddXmlSerializerFormatters().AddJsonOptions(options => {
-                options.JsonSerializerOptions.IgnoreNullValues = true;
-            });
+            services
+                .AddControllers(opt => opt.OutputFormatters.Add(new HtmlMediaTypeFormatter()))
+                .AddXmlSerializerFormatters().
+                AddJsonOptions(options => {options.JsonSerializerOptions.IgnoreNullValues = true;});
 
             services.AddSingleton(Configuration);
         }
