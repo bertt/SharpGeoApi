714dfddd220b1f60d44810367cb874d0ef6aa73e
bertt <btemme@hotmail.com>
2019-10-21 20:17:32 +0200

    add RespectBorowserAcceptHeader

--- src/SharpGeoApi/SharpGeoApi/Startup.cs
+++ src/SharpGeoApi/SharpGeoApi/Startup.cs
@@ -23,7 +23,9 @@ namespace SharpGeoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.IgnoreNullValues = true; });
+            services.AddControllers(options => {
+                options.RespectBrowserAcceptHeader=true;
+            }).AddJsonOptions(options => { options.JsonSerializerOptions.IgnoreNullValues = true; });
 
             services.AddMvc(options =>
             {
